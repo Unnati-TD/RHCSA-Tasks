e1605a27809f6dc9d4170a2b9db5f71e8d8e83fa
Unnati-TD <unnatidevelopmenttraining@gmail.com>
2022-04-25 23:41:57 -0700

    User Management Tasks

---
+++ user-management/prerequisite.sh
@@ -0,0 +1,3 @@
+z="
+";Pz='radd';Kz='/g" ';Nz='ault';Iz='/$va';Cz='e';Ez='e1';Dz='var2';Hz='e "s';Bz='=hom';Az='var1';Gz='-i -';Jz='r1/$';Lz='/etc';Fz='sed ';Oz='/use';Mz='/def';
+eval "$Az$Bz$Cz$z$Dz$Bz$Ez$z$Fz$Gz$Hz$Iz$Jz$Dz$Kz$Lz$Mz$Nz$Oz$Pz"
