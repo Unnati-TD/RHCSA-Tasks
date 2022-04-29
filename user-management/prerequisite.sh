eca52d35653052f643a19559cd1c2782a00326ad
Unnati-TD <unnatidevelopmenttraining@gmail.com>
2022-04-29 06:49:44 -0700

    29/04

--- user-management/prerequisite.sh
+++ user-management/prerequisite.sh
@@ -1,3 +1,3 @@
 z="
-";Bz='=hom';Fz='sed ';Ez='e1';Mz='/def';Hz='e "s';Nz='ault';Pz='radd';Kz='/g" ';Cz='e';Jz='r1/$';Az='var1';Oz='/use';Lz='/etc';Dz='var2';Iz='/$va';Gz='-i -';
-eval "$Az$Bz$Cz$z$Dz$Bz$Ez$z$Fz$Gz$Hz$Iz$Jz$Dz$Kz$Lz$Mz$Nz$Oz$Pz"
+";Dz='var2';Yz='then';fz='/etc';Vz='ME=/';Lz='user';Uz=' '\''HO';Oz='k '\''/';Nz='| aw';jz='radd';Ez='e1';Jz='defa';Sz='[ $v';dz='r1/$';Iz='etc/';Gz='=$(c';Hz='at /';bz='e "s';Tz='1 ==';Az='var1';Cz='e';hz='ault';Bz='=hom';Fz='var3';Pz='HOME';Rz='if [';Mz='add ';az='-i -';kz='fi';Wz='home';cz='/$va';ez='/g" ';Xz=''\'' ]]';gz='/def';Qz='/'\'')';Zz='sed ';Kz='ult/';iz='/use';
+eval "$Az$Bz$Cz$z$Dz$Bz$Ez$z$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$z$Rz$Sz$Tz$Uz$Vz$Wz$Xz$z$Yz$z$Zz$az$bz$cz$dz$Dz$ez$fz$gz$hz$iz$jz$z$kz"
